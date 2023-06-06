7ad258885d2307ae2b25c4175718583ec22f1328
Achmad Adyatma Ardi <adyatmaardi@gmail.com>
2023-06-06 17:55:07 +0700

    Add files via upload

---
+++ css-grid/css-grid.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="css/css-grid.css">
+    </head>
+    <body>
+        <div class="">
+            <p>grid-template-columns & grid-template-rows</p>
+            <img src="img/sc1.png">
+        </div>
+        <div class="">
+            <p>Explicit and implicit grid</p>
+            <img src="img/sc2.png">
+        </div>
+        <div class="">
+            <p>grid-auto-columns & grid-auto-rows</p>
+            <img src="img/sc3.png">
+        </div>
+        <div class="">
+            <p>grid-auto-flow</p>
+            <img src="img/sc4.png">
+        </div>
+        <div class="">
+            <p>grid value</p>
+            <img src="img/sc5.png">
+        </div>
+        <div class="">
+            <p>fraction</p>
+            <img src="img/sc6.png">
+        </div>
+        <div class="">
+            <p>repeat</p>
+            <img src="img/sc7.png">
+        </div>
+        <div class="">
+            <p>min-max content</p>
+            <img src="img/sc8.png">
+            <img src="img/sc9.png" alt="">
+        </div>
+        <div class="">
+            <p>auto-fill & auto-fit</p>
+            <img src="img/sc10.png">
+            <img src="img/sc11.png">
+        </div>
+        <div class="">
+            <p>minmax</p>
+            <img src="img/sc7.png">
+        </div>
+    </body>
+</html>
