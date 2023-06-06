4f9f41c598d269bab88f1622ab1be6b7686df6f9
Achmad Adyatma Ardi <adyatmaardi@gmail.com>
2023-06-06 18:16:46 +0700

    Add files via upload

---
+++ php-basic/18_PDF_REPORTING/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    // hilangkan tombol cari
+    $('#tombol-cari').hide();
+
+    // event ketika keyword ditulis
+    $('#keyword').on('keyup', function () {
+
+        // munculkan icon loading
+        $('.loader').show();
+
+        // ajax menggunakan load
+        // $('#container').load('ajax/mahasiswa.php?keyword=' + $('#keyword').val());
+
+        // ajax menggunakan $.get()
+        $.get('ajax/mahasiswa.php?keyword=' + $('#keyword').val(), function (data) {
+
+            // function (data) => menggantikan XMLHttpRequest
+            $('#container').html(data);
+            $('.loader').hide();
+        });
+
+    });
+
+});
