b26dfb9901cd2ef1804a58a37702781987e42f87
Achmad Adyatma Ardi <adyatmaardi@gmail.com>
2023-06-06 18:01:14 +0700

    Add files via upload

---
+++ php-basic/13_SESSION/index.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+
+if ( !isset($_SESSION["login"]) ) {
+    header("Location: login.php");
+    exit;
+}
+
+
+// function
+require 'functions2.php';
+
+// Query by default
+$mahasiswa = query("SELECT * FROM mahasiswa ORDER BY id ASC");
+
+// Tombol cari ditekan
+if( isset($_POST["cari"]) ) {
+    $mahasiswa = cari($_POST["keyword"]);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <a href="logout.php">Log out</a>
+    <h2>Daftar Mahasiswa</h2>
+
+    <br>
+    <p><a href="tambah_modular.php">tambah data mahasiswa (modular)</a></p>
+    <p><a href="tambah_nonmodular.php">tambah data mahasiswa (non_modular)</a></p>
+    <br>
+
+    <form action="" method="post">
+        <input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian..." autocomplete="off">
+        <button type="submit" name="cari">Cari!</button>
+    </form>    
+    
+    <br>
+
+    <table border="1" cellpadding="5" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+
+        <!-- lakukan perulangan untuk fetch data -->
+        <?php $i = 1; ?>
+        <?php foreach($mahasiswa as $row): ?>
+        <tr>
+            <td style="text-align:center;"><?= $i;?></td>
+            <td>
+                <a href="ubah.php?id=<?= $row["id"];?>">change</a> |
+                <a href="hapus.php?id=<?= $row["id"];?>" onclick="return confirm('yakin ?');">delete</a>
+            </td>
+            <td><img src="image_dir/<?= $row["gambar"]; ?>" width="100" alt=""></td>
+            <td><?= $row["nrp"]; ?></td>
+            <td><?= $row["nama"]; ?></td>
+            <td><?= $row["email"]; ?></td>
+            <td><?= $row["jurusan"]; ?></td>
+        </tr>
+        <?php $i++;?>
+        <?php endforeach; ?>
+        
+    </table>
+
+</body>
+</html>
---
+++ php-basic/13_SESSION/registrasi.php
@@ -0,0 +1,60 @@
+<?php 
+ini_set("display_errors","1");
+ini_set("display_startup_errors","1");
+error_reporting(E_ALL);
+
+require 'functions2.php';
+
+if( isset($_POST["register"]) ) {
+    if( registrasi($_POST) > 0 ) {
+        echo "
+            <script>
+                alert('user baru berhasil ditambahkan!');
+            </script>
+            ";
+    } else {
+        echo "
+            <script>
+                alert('user baru gagal ditambahkan!');
+            </script>
+            ";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman registrasi</title>
+    <style>
+        label {
+            display:block;
+        }
+    </style>
+</head>
+<body>
+    <h2>Halaman registrasi</h2>
+    <form action="" method="post">
+    <ul>
+        <li>
+            <label for="username">username :</label>
+            <input type="text" name="username" id="username">
+        </li>
+        <li>
+            <label for="password">password :</label>
+            <input type="password" name="password" id="password">
+        </li>
+        <li>
+            <label for="password2">konfirmasi password :</label>
+            <input type="password" name="password2" id="password2">
+        </li>
+        <li>
+            <button type="submit" name="register">Sign up!</button>
+        </li>
+    </ul>
+    </form>
+</body>
+</html>
