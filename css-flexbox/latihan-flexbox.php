3cb85683ec190064f22d3a77a8ba1858fdc1149a
Achmad Adyatma Ardi <adyatmaardi@gmail.com>
2023-06-06 17:49:33 +0700

    Add files via upload

---
+++ css-flexbox/latihan-flexbox.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Latihan-flexbox</title>
+        <link rel="stylesheet" href="assets/css/latihanstyle.css">
+    </head>
+    <body>
+        <!-- Latihan 1 - kolom -->
+        <div class="container-satu">
+            <div class="kolom-utama">
+                <h2>Judul Artikel</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="sidebar-satu">
+                <h3>Sidebar satu</h3>
+                <ul>
+                    <li><a href="#">Link 1</a></li>
+                    <li> <a href="#">Link 2</a> </li>
+                    <li> <a href="#">Link 3</a> </li>
+                    <li> <a href="#">Link 4</a> </li>
+                    <li> <a href="#">Link 5</a> </li>
+                </ul>
+            </div>
+            <div class="sidebar-dua">
+                <h3>Sidebar dua</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+
+        <!-- Latihan 2 - Service / Features -->
+        <div class="container-dua">
+            <div class="layanan satu">
+                <h4>Layanan 1</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="layanan dua">
+                <h4>Layanan 2</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="layanan tiga">
+                <h4>Layanan 3</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+
+        <!-- Latihan 3 - Galery -->
+        <div class="container-tiga">
+            <div class="thumbnail">
+                <img src="assets/img/kopi/kopi_1.jpeg">
+            </div>
+            <div class="thumbnail">
+                <img src="assets/img/kopi/kopi_2.jpeg">
+            </div>
+            <div class="thumbnail">
+                <img src="assets/img/kopi/kopi_3.jpeg">
+            </div>
+            <div class="thumbnail">
+                <img src="assets/img/kopi/kopi_4.jpeg">
+            </div>
+            <div class="thumbnail">
+                <img src="assets/img/kopi/kopi_5.jpeg">
+            </div>
+            <div class="thumbnail">
+                <img src="assets/img/kopi/kopi_6.jpeg">
+            </div>
+            <div class="thumbnail">
+                <img src="assets/img/kopi/kopi_7.jpeg">
+            </div>
+        </div>
+
+        <!-- Latihan 4 - Card -->
+        <div class="container-empat">
+            <div class="card">
+                <div class="header"></div>
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+                    <button>Buy Now!</button>
+                </div>
+            </div>
+            <div class="card besar">
+                <div class="header"></div>
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+                    <button>Buy Now!</button>
+                </div>
+            </div>
+            <div class="card">
+                <div class="header"></div>
+                <div class="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+                    <button>Buy Now!</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Latihan 5 - Centering -->
+        <div class="container-lima">
+            <div class="kotak">
+                <span>Hello <br>Achmad Adyatma Ardi !</span>
+            </div>
+        </div>
+
+    </body>
+</html>
