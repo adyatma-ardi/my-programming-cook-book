d34d496e5e7898f23539198ba6750ae4e568536a
Achmad Adyatma Ardi <adyatmaardi@gmail.com>
2023-06-06 18:13:23 +0700

    Add files via upload

---
+++ php-basic/17_LIVE_SEARCH_jQuery/ajax/mahasiswa.php
@@ -0,0 +1,47 @@
+<?php 
+sleep(1);
+// usleep(500000); // 0.5 detik
+require '../functions2.php';
+
+$keyword = $_GET['keyword'];
+$query ="SELECT * FROM mahasiswa
+         WHERE
+            nama LIKE '%$keyword%' OR
+            nrp LIKE '%$keyword%' OR
+            email LIKE '%$keyword%' OR
+            jurusan LIKE '%$keyword%'";
+
+$mahasiswa = query($query);
+
+?>
+
+<table border="1" cellpadding="5" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+
+        <!-- lakukan perulangan untuk fetch data -->
+        <?php $i = 1; ?>
+        <?php foreach($mahasiswa as $row): ?>
+        <tr>
+            <td style="text-align:center;"><?= $i;?></td>
+            <td>
+                <a href="ubah.php?id=<?= $row["id"];?>">change</a> |
+                <a href="hapus.php?id=<?= $row["id"];?>" onclick="return confirm('yakin ?');">delete</a>
+            </td>
+            <td><img src="image_dir/<?= $row["gambar"]; ?>" width="100" alt=""></td>
+            <td><?= $row["nrp"]; ?></td>
+            <td><?= $row["nama"]; ?></td>
+            <td><?= $row["email"]; ?></td>
+            <td><?= $row["jurusan"]; ?></td>
+        </tr>
+        <?php $i++;?>
+        <?php endforeach; ?>
+        
+</table>
---
+++ php-basic/17_LIVE_SEARCH_jQuery/ubah.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+
+if ( !isset($_SESSION["login"]) ) {
+    header("Location: login.php");
+    exit;
+}
+
+ini_set("display_errors","1");
+ini_set("display_startup_errors","1");
+error_reporting(E_ALL);
+
+require 'functions2.php';
+
+// ambil data di URL
+$id = $_GET["id"];
+
+// query data mahasiswa berdasarkan id
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
+
+// cek apakah tombol submit sudah ditekan atau belum
+if( isset($_POST["submit"]) ) {
+    // cek apakah data berhasil diubah atau tidak
+    if( ubah($_POST) > 0 ){
+        echo "
+            <script>
+                alert('data berhasil diubah!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    } else {
+        echo "
+            <script>
+                alert('data gagal diubah!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="w2idth=device-width, initial-scale=1.0">
+    <title>Ubah data mahasiswa</title>
+</head>
+<body>
+    <h2>Ubah data mahasiswa</h2>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="<?= $mhs["id"]; ?>">
+        <input type="hidden" name="gambarLama" value="<?= $mhs["gambar"];?>">
+        <ul>
+            <li>
+                <label for="nrp">NRP :</label>
+                <input type="text" name="nrp" id="nrp" required value="<?= $mhs["nrp"]; ?>">
+            </li>
+            <li>
+                <label for="nama">Nama :</label>
+                <input type="text" name="nama" id="nama" value="<?= $mhs["nama"]; ?>">
+            </li>
+            <li>
+                <label for="email">Email :</label>
+                <input type="text" name="email" id="email" value="<?= $mhs["email"]; ?>">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan :</label>
+                <input type="text" name="jurusan" id="jurusan" value="<?= $mhs["jurusan"]; ?>">
+            </li>
+            <li>
+                <label for="gambar">Gambar :</label> <br>
+                <img src="image_dir/<?= $mhs['gambar'];?>" width="100"> <br>
+                <input type="file" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Ubah data!</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
