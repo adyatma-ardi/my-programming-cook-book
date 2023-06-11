74693cc2e8979e7dbcc7ddf994296eb24a4124ab
adyatma-ardi <adyatmaardi@gmail.com>
2023-06-12 00:01:26 +0700

    popup box javascript

---
+++ javascript/popup.js
@@ -0,0 +1,12 @@
+// alert
+// alert("Hello World!");
+// alert('1');
+// alert('2');
+
+// prompt
+// var nama = prompt("Masukkan nama");
+// alert(nama);
+
+// confirm
+// var yakin = confirm("Kamu yakin ?");
+// alert(yakin);
