96eedb79d9411e324476e4b7872cdcb397a0c195
adyatma-ardi <adyatmaardi@gmail.com>
2023-07-30 17:44:32 +0700

    perbedaan penggunaan keyword var, let, dan const dalam JavaScript

---
+++ javascript/var, let, const/script.js
@@ -0,0 +1,43 @@
+// console.log(i);
+// var i = 0;
+
+// console.log(j);
+// let j = 0;
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+// for (let j = 0; j < 10; j++) {
+//     console.log(j);
+// }
+// console.log(j);
+
+
+
+
+
+// const i = 10;
+// i = 15
+// console.log(i);
+
+// const j = {
+//     nama: 'Achmad',
+//     umur: 26
+// }
+// j.umur = 27;
+// console.log(j);
+
+// const j = {
+//     name: 'Achmad',
+//     age: 26;
+// }
+// console.log(j);
+
+// const k = [1, 2, 3];
+// k.push(4);
+// console.log(k);
+
+// const k = [5, 4, 1997];
+// console.log(k);
